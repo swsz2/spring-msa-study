cc7e22d6e57072a06221aaea71780a7e80543238
swsz2 <swsz_@naver.com>
2021-11-14 21:05:26 +0900

    add bookstore

---
+++ bookstore/src/main/java/me/swsz2/cloud/bookstore/BookstoreApplication.java
@@ -0,0 +1,13 @@
+package me.swsz2.cloud.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreApplication.class, args);
+    }
+
+}
