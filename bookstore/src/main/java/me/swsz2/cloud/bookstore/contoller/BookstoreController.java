5b48cf261e9e267aae5205aded32b6c35cefc98e
swsz2 <swsz_@naver.com>
2021-11-15 11:52:05 +0900

    Fix Search API in BookstoreController

--- bookstore/src/main/java/me/swsz2/cloud/bookstore/contoller/BookstoreController.java
+++ bookstore/src/main/java/me/swsz2/cloud/bookstore/contoller/BookstoreController.java
@@ -6,11 +6,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
+import java.util.Random;
 
 @RestController
 public class BookstoreController {
+
+  private final Random RANDOM = new Random();
+  private final int MAX_ID = 10;
+  private final int MIN_ID = 1;
+  private final int MAX_AMOUNT = 10;
+
   @GetMapping(value = "/api/v1/search")
   public SearchResult search(@RequestParam(name = "title") final Optional<String> title) {
-    return title.map(s -> new SearchResult(1, s, 1)).orElseGet(SearchResult::dummy);
+    return title
+        .map(
+            s ->
+                new SearchResult(
+                    RANDOM.nextInt(MAX_ID) + MIN_ID, s, RANDOM.nextInt(MAX_AMOUNT) + MIN_ID))
+        .orElseGet(SearchResult::dummy);
   }
 }
