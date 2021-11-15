6c175c568c20386064f8cc690e09904786c65716
swsz2 <swsz_@naver.com>
2021-11-15 11:50:41 +0900

    Create ExceptionResponse

---
+++ gateway/src/main/java/me/swsz2/cloud/gateway/vo/ExceptionResponse.java
@@ -0,0 +1,19 @@
+package me.swsz2.cloud.gateway.vo;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.util.Date;
+
+/** Created by sungwonkim on 2021-11-15 */
+@Data
+@Builder
+public class ExceptionResponse {
+  private final Date timestamp = new Date();
+  private String message;
+  private InetSocketAddress address;
+  private String method;
+  private URI path;
+}
