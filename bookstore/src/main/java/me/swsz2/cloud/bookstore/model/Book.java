fbfa0a77180490c771f39f7bedbad6069d8bf728
swsz2 <swsz_@naver.com>
2021-11-16 17:40:53 +0900

    Add NoArgsConstructor on Book

--- bookstore/src/main/java/me/swsz2/cloud/bookstore/model/Book.java
+++ bookstore/src/main/java/me/swsz2/cloud/bookstore/model/Book.java
@@ -2,10 +2,12 @@ package me.swsz2.cloud.bookstore.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /** Created by sungwonkim on 2021-11-16 */
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Book {
   private long id;
   private String title;
