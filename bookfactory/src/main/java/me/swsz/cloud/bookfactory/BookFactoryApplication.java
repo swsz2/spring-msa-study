b064c2dc2c7e62e253f7ef61045d085d6a10e6c5
swsz2 <swsz_@naver.com>
2021-11-24 12:07:01 +0900

    Create BookFactory

---
+++ bookfactory/src/main/java/me/swsz/cloud/bookfactory/BookFactoryApplication.java
@@ -0,0 +1,13 @@
+package me.swsz.cloud.bookfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookFactoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookFactoryApplication.class, args);
+    }
+
+}
